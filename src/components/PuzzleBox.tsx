777e80b63db6426f4afabda073ec72960a47cfcc
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-26 21:18:54 +0100

    resolving pushing issues

---
+++ src/components/PuzzleBox.tsx
@@ -0,0 +1,29 @@
+import { useGLTF } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { useControls } from 'leva'
+
+export default function PuzzleBox(){
+    const model = useGLTF('./lament-optimised2.glb')    
+
+    const { intensity } = useControls({
+        intensity: {
+            value: 3,
+            min: 0,
+            max: 10,
+            step: 0.5
+        }
+    })
+
+    return(
+        <>
+            <Perf position="top-left" />
+
+            <OrbitControls makeDefault />
+            <directionalLight castShadow  position={ [ 1, 2, 3 ] } intensity={ intensity } />
+            <ambientLight intensity={ intensity }/>
+            <primitive object={model.scene} scale={20} />
+        </>
+    )
+
+}
