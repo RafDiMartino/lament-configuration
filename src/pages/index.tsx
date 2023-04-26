0d5d6046261303fb894ca82f8309b4ca13c24c12
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-26 14:15:12 +0100

    adding favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 
 export default function Home() {
   return (
-    <main>
-      <div className='flex justify-center items-center'>Initial Setup</div>
-    </main>
+    <>
+
+      <Head>
+        <title>Lament Configuration</title>
+        <meta name="description" content="Raf Di Martino | Web Development Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      
+      <main>
+        <div className='flex justify-center items-center'>Initial Setup</div>
+      </main>
+    </>
+
   )
 }
