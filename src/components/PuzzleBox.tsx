b6aaa51c5c3a8f19ca6f446e238b84dbc88bd382
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-04-29 21:23:14 +0100

    testing useTexture  with model with no material

--- src/components/PuzzleBox.tsx
+++ src/components/PuzzleBox.tsx
@@ -1,35 +1,36 @@
-import { useGLTF } from '@react-three/drei'
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls, useHelper, useTexture, Plane } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { useControls } from 'leva'
 import LamentConfig from './LamentConfig'
+import { PointLight, PointLightHelper, DirectionalLight, DirectionalLightHelper, LinearEncoding } from 'three'
+import { useRef, Suspense } from 'react'
 
 export default function PuzzleBox(){
-    // const model = useGLTF('./lament-final-nom-opt.glb')    
-    // const modelFlat = useGLTF('./lament-final-opt.glb')    
+    const model = useGLTF('./lament-final-nom-opt.glb')    
 
-    // const { intensity } = useControls({
-    //     intensity: {
-    //         value: 3,
-    //         min: 0,
-    //         max: 10,
-    //         step: 0.5
-    //     }
-    // })
+    const lightRef = useRef<PointLight>(null!)
+    const dirLightRef = useRef<DirectionalLight>(null!)
+    useHelper(lightRef, PointLightHelper, 1, "red")
+    useHelper(dirLightRef, DirectionalLightHelper, 1, "red")
 
     return(
         <>
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
-            <directionalLight castShadow  position={ [ 1, 2, 3 ] } intensity={ 5 } />
-            <directionalLight castShadow  position={ [ -1, 2, -3 ] } intensity={ 5 } />
-            {/* <ambientLight intensity={ 3 }/> */}
+            <pointLight ref={lightRef} castShadow  position={ [ 5, 5, 0 ] } intensity={ 1.5 } shadow-normalBias={0.04}/>
+            {/* <directionalLight ref={dirLightRef} castShadow  position={ [ -1, 7, -5 ] } intensity={ 5 } shadow-normalBias={0.04}/> */}
+
             {/* <primitive object={model.scene} scale={1} >
-                <meshStandardMaterial color="red" transparent />
+                <meshStandardMaterial
+                    map={woodTexture}
+                    attach='background'
+                />
             </primitive> */}
-            {/* <primitive object={modelFlat.scene} scale={1} /> */}
-            <LamentConfig/>
+            <Suspense fallback={null}>
+                <LamentConfig/>
+            </Suspense>
+
         </>
     )
 
