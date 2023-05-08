e55e3951d421b689e429c43674c12326b94ab443
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-05-08 16:39:38 +0100

    adding next/image to loaders component

--- src/components/Loaders.tsx
+++ src/components/Loaders.tsx
@@ -1,4 +1,5 @@
 import { Html, useProgress, Loader } from '@react-three/drei'
+import Image from 'next/image'
 
 export default function Loaders(){
 
@@ -10,7 +11,7 @@ export default function Loaders(){
             <div className="w-32">
                 <Loader containerStyles={{backgroundColor: "rgba(255, 255, 255, 0)", position: "absolute", top: "100px"}}/>
                 <span className="hidden">{ progress }</span>
-                <img className='loader z-20' src="./box.png" alt="Loader" />
+                <Image width={150} height={150} className='loader z-20' src="/box.png" alt="Loader" />
             </div>
         </Html>
     )
