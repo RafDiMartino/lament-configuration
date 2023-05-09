2ffb325cbc2f71f19533b4a29a27d533690e1f06
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-05-09 17:46:42 +0100

    adding priority to Image tag

--- src/components/Loaders.tsx
+++ src/components/Loaders.tsx
@@ -11,7 +11,7 @@ export default function Loaders(){
             <div className="w-32">
                 <Loader containerStyles={{backgroundColor: "rgba(255, 255, 255, 0)", position: "absolute", top: "100px"}}/>
                 <span className="hidden">{ progress }</span>
-                <Image width={150} height={150} className='loader z-20' src="/box.png" alt="Loader" />
+                <Image priority width={150} height={150} className='loader z-20' src="/box.png" alt="Loader" />
             </div>
         </Html>
     )
